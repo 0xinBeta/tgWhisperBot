5339ad23e5f04d09ad5f975b8c338340bbb40199
0xinBeta <xterminbeta@protonmail.com>
2023-10-09 18:28:24 +0300

    add openAI 11Labs

--- tg_bot_handler.py
+++ tg_bot_handler.py
@@ -3,16 +3,25 @@ from telegram import Update, ForceReply
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 import keys
 import whisper
+import tempfile
+from io import BytesIO
+import openai
+from elevenlabs import set_api_key, generate
 
-model = whisper.load_model("base")
+
+model = whisper.load_model("medium")
 
 token = keys.BOT_TOKEN
+openai.api_key = keys.OPENAI_API
+set_api_key(f"{keys.ELEVEN_API}")
+
 
 # Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
+
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a message when the command /hello is issued."""
@@ -22,19 +31,42 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         reply_markup=ForceReply(selective=True),
     )
 
+
 async def handle_voice(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Handle voice messages."""
     voice = update.message.voice
     voice_file = await voice.get_file()
 
-    download_path = "audio.ogg"
+    voice_io = BytesIO()
+    # download_path = "audio.ogg"
+    await voice_file.download_to_memory(out=voice_io)
+    voice_io.seek(0)
 
-    await voice_file.download_to_drive(download_path)
-    
-    result = model.transcribe("audio.ogg") 
+    with tempfile.NamedTemporaryFile(suffix=".ogg", delete=True) as tmp_file:
+        tmp_file.write(voice_io.read())
+        tmp_file.flush()
 
-    await update.message.reply_text(f"{result['text']}")
+        result = model.transcribe(tmp_file.name, fp16=False)
 
+    question = result['text']
+
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": f"{question}"}
+        ]   
+    )
+
+    bot_resp = completion.choices[0].message.content
+
+    audio = generate(
+        text=f"{bot_resp}",
+        voice="Bella",
+        model="eleven_multilingual_v2"
+    )
+
+    await update.message.reply_audio(audio=audio)
 
 
 def main() -> None:
@@ -49,6 +81,7 @@ def main() -> None:
 
     # Run the bot until the user presses Ctrl-C
     application.run_polling(allowed_updates=Update.ALL_TYPES)
+
 
 if __name__ == "__main__":
     main()
